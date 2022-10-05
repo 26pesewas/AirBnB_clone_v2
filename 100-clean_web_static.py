2c4126c353fbd2917ef81e9008e50f4ead995186
26pesewas <naaakuokor@gmail.com>
2022-10-04 17:04:29 -0700

    task 4

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -17,7 +17,7 @@ from datetime import datetime
 import os
 import os.path
 
-env.hosts = ["3.236.44.83", "44.200.29.105"]
+env.hosts = ["44.200.178.214", "35.174.60.115"]
 env.user = 'ubuntu'
 
 
@@ -36,7 +36,7 @@ def do_pack():
 
 
 def do_deploy(archive_path):
-    """ function distrubtes an archive to my web servers
+    """ function distributes an archive to my web servers
     """
     path_existence = os.path.exists(archive_path)
     if path_existence is False:
@@ -47,15 +47,15 @@ def do_deploy(archive_path):
         no_tgz_name = path_split[1]
         full_filename = path_split[1] + '.' + path_split[2]
         folder = '/data/web_static/releases/{}/'.format(no_tgz_name)
-        put(archive_path, '/tmp/')
-        run('mkdir -p {}'.format(folder))
-        run('tar -xzf /tmp/{} -C {}/'.format(full_filename, folder))
-        run('rm /tmp/{}'.format(full_filename))
-        run('mv {}/web_static/* {}'.format(folder, folder))
-        run('rm -rf {}/web_static'.format(folder))
+        sudo(put(archive_path, '/tmp/'))
+        sudo(run('mkdir -p {}'.format(folder)))
+        sudo(run('tar -xzf /tmp/{} -C {}/'.format(full_filename, folder)))
+        sudo(run('rm /tmp/{}'.format(full_filename)))
+        sudo(run('mv {}/web_static/* {}'.format(folder, folder)))
+        sudo(run('rm -rf {}/web_static'.format(folder)))
         current = '/data/web_static/current'
-        run('rm -rf {}'.format(current))
-        run('ln -s {}/ {}'.format(folder, current))
+        sudo(run('rm -rf {}'.format(current)))
+        sudo(run('ln -s {}/ {}'.format(folder, current)))
         return True
     except Exception:
         return False
