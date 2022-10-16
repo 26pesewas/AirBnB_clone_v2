6bc91c186494d73752ba89c4542f7f4c5cb51b3f
26pesewas <naaakuokor@gmail.com>
2022-10-16 14:31:03 -0700

    deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -21,29 +21,28 @@ def do_deploy(archive_path):
     file = archive_path.split("/")[-1]
     name = file.split(".")[0]
 
-    if sudo(put(archive_path, "/tmp/{}").format(file)).failed is True:
+    if put(archive_path, "/tmp/{}".format(file)).failed is True:
         return False
-    if sudo(run("rm -rf /data/web_static/releases/{}/").
-            format(name)).failed is True:
+    if run("rm -rf /data/web_static/releases/{}/".
+           format(name)).failed is True:
         return False
-    if sudo(run("mkdir -p /data/web_static/releases/{}/").
-            format(name)).failed is True:
+    if run("mkdir -p /data/web_static/releases/{}/".
+           format(name)).failed is True:
         return False
-    if sudo(run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
-            format(file, name))).failed is True:
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+           format(file, name)).failed is True:
         return False
-    if sudo(run("rm /tmp/{}".format(file))).failed is True:
+    if run("rm /tmp/{}".format(file)).failed is True:
         return False
-    if sudo(run("mv /data/web_static/releases/{}/web_static/* "
-            "/data/web_static/releases/{}/".
-                format(name, name))).failed is True:
+    if run("mv /data/web_static/releases/{}/web_static/* "
+           "/data/web_static/releases/{}/".format(name, name)).failed is True:
         return False
-    if sudo(run("rm -rf /data/web_static/releases/{}/web_static".
-            format(name))).failed is True:
+    if run("rm -rf /data/web_static/releases/{}/web_static".
+           format(name)).failed is True:
         return False
-    if sudo(run("rm -rf /data/web_static/current")).failed is True:
+    if run("rm -rf /data/web_static/current").failed is True:
         return False
-    if sudo(run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-            format(name))).failed is True:
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+           format(name)).failed is True:
         return False
     return True
