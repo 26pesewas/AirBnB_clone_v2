18dd84d3afc76ee811ae2e41c5fac2c4141353e2
26pesewas <naaakuokor@gmail.com>
2022-10-16 14:20:13 -0700

    deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,6 +7,7 @@ from fabric.api import run
 
 env.hosts = ["44.200.178.214", "35.174.60.115"]
 
+
 def do_deploy(archive_path):
     """ This is to send an archive to a web server.
     Args:
@@ -20,28 +21,29 @@ def do_deploy(archive_path):
     file = archive_path.split("/")[-1]
     name = file.split(".")[0]
 
-    if put(archive_path, "/tmp/{}".format(file)).failed is True:
+    if sudo(put(archive_path, "/tmp/{}").format(file)).failed is True:
         return False
-    if run("rm -rf /data/web_static/releases/{}/".
-           format(name)).failed is True:
+    if sudo(run("rm -rf /data/web_static/releases/{}/").
+            format(name)).failed is True:
         return False
-    if run("mkdir -p /data/web_static/releases/{}/".
-           format(name)).failed is True:
+    if sudo(run("mkdir -p /data/web_static/releases/{}/").
+            format(name)).failed is True:
         return False
-    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
-           format(file, name)).failed is True:
+    if sudo(run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+            format(file, name))).failed is True:
         return False
-    if run("rm /tmp/{}".format(file)).failed is True:
+    if sudo(run("rm /tmp/{}".format(file))).failed is True:
         return False
-    if run("mv /data/web_static/releases/{}/web_static/* "
-           "/data/web_static/releases/{}/".format(name, name)).failed is True:
+    if sudo(run("mv /data/web_static/releases/{}/web_static/* "
+            "/data/web_static/releases/{}/".
+                format(name, name))).failed is True:
         return False
-    if run("rm -rf /data/web_static/releases/{}/web_static".
-           format(name)).failed is True:
+    if sudo(run("rm -rf /data/web_static/releases/{}/web_static".
+            format(name))).failed is True:
         return False
-    if run("rm -rf /data/web_static/current").failed is True:
+    if sudo(run("rm -rf /data/web_static/current")).failed is True:
         return False
-    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-           format(name)).failed is True:
+    if sudo(run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+            format(name))).failed is True:
         return False
     return True
